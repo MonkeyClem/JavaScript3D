eee69c04e9169e38d2a1d6c1f5f8a83d4169c898
Clement Jeulin <clement93jeulin@gmail.com>
2023-06-05 13:41:40 +0200

    2nd Commit

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import * as THREE from 'three';
+
+import { Scene, WebGLRenderer, AxesHelper } from 'three';
+import './styles.css';
+
+const scene = new Scene();
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+document.body.appendChild(renderer.domElement)
+
